0a53020cd955ab3359f96f4958f0b094dadf73bc
sooraj <sooraj1998ayiroor@gmail.com>
2020-10-31 17:47:36 +0530

    carousel error

--- src/components/carousel.component.js
+++ src/components/carousel.component.js
@@ -1,10 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import '../styles/App.css';
+
+const slideImages = [
+  '../asset/banner1.jpg',
+  '../asset/banner2.jpg',
+  '../asset/banner3.jpg'
+];
 
 export default class Carousalpage extends Component {
   render() {
     return (
-      <div>
-        Carousel
+      <div className="slide-container">
+        <Slide>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+              <span>Slide 1</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+              <span>Slide 2</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+              <span>Slide 3</span>
+            </div>
+          </div>
+        </Slide>
       </div>
     )
   }
--- src/pages/app.js
+++ src/pages/app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from '../components/navbar';
 import Routes from '../routes/routes';
 import Carouselpage from "../components/carousel.component";
-
 const App = () => (
   <>
     <Navbar />
